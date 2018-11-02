3c0ab5692f7c6deea1eb6488e9c90ac80a56dfb9
Dmitry Kozubov <37287662+kozubov@users.noreply.github.com>
2018-11-02 10:56:06 +0200

    adding app

---
+++ WPF_3/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Person> Persons { set; get; }
+    public Person MyPerson { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Persons = new ObservableCollection<Person>
+            {
+                new Person{ FirstName = "Михаил", Surname="Светлов", Age = "44"},
+                new Person{ FirstName = "Сергей", Surname="Есенин", Age = "43"},
+                new Person{ FirstName = "Александр", Surname="Пушкин", Age = "42"}
+            };
+            DataContext = Persons;
+            persons_list.ItemsSource = Persons;
+        }
+
+        private void Persons_list_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (persons_list.SelectedIndex != -1)
+            {
+                Name.Text = Persons[persons_list.SelectedIndex].FirstName;
+                Surname.Text = Persons[persons_list.SelectedIndex].Surname;
+                Age.Text = Persons[persons_list.SelectedIndex].Age;
+            }
+        }
+
+        private void Add(object sender, RoutedEventArgs e)
+        {
+
+            if (Name.Text == "" || Surname.Text == "" || Age.Text == "")
+            {
+                MessageBox.Show("Заполните поля!", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            try
+            {
+                int age = Convert.ToInt32(Age.Text);
+                Persons.Add(new Person { FirstName = Name.Text, Surname = Surname.Text, Age = Age.Text });
+                Name.Text = null;
+                Surname.Text = null;
+                Age.Text = null;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Правильно вводите данные!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Delete(object sender, RoutedEventArgs e)
+        {
+            if (persons_list.SelectedIndex != -1)
+            {
+                Persons.RemoveAt(persons_list.SelectedIndex);
+                Name.Text = null;
+                Surname.Text = null;
+                Age.Text = null;
+            }
+            else
+            {
+                MessageBox.Show("Выберите сотрудника для удаления!", "Оповещение", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void Edit(object sender, RoutedEventArgs e)
+        {
+            if (persons_list.SelectedIndex != -1)
+            {
+                if (Name.Text == "" || Surname.Text == "" || Age.Text == "")
+                {
+                    MessageBox.Show("Введите все поля!", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return;
+                }
+                try
+                {
+                    int Age = Convert.ToInt32(this.Age.Text);
+                    Persons[persons_list.SelectedIndex].FirstName = this.Name.Text;
+                    Persons[persons_list.SelectedIndex].Surname = this.Surname.Text;
+                    Persons[persons_list.SelectedIndex].Age = this.Age.Text;
+                    Person s = (Person)this.Resources["stuff"];
+                    s.FirstName = this.Name.Text;
+                    s.Surname = this.Surname.Text;
+                    s.Age = this.Age.Text;
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Не коректный ввод данных!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Выберите сотрудника для изменения!", "Оповещение", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+    }
+}
---
+++ WPF_3/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_3
+{
+    public class Person : INotifyPropertyChanged
+    {
+        private string firstName;
+        private string surname;
+        private string age;
+        public string FirstName
+        {
+            set
+            {
+                firstName = value;
+                OnPropertyChanged("FirstName");
+            }
+            get
+            {
+                return firstName;
+            }
+        }
+        public string Surname
+        {
+            set
+            {
+                surname = value;
+                OnPropertyChanged("Surname");
+            }
+            get
+            {
+                return surname;
+            }
+        }
+
+        public string Age
+        {
+            set
+            {
+                age = value;
+                OnPropertyChanged("Age");
+            }
+            get
+            {
+                return age;
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName]string name = "")
+        {
+            //if (PropertyChanged != null)
+            //{
+            //    PropertyChanged(this, new PropertyChangedEventArgs(name));
+            //}
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        }
+    }
+}
